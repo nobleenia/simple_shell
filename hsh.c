858a256ac0aca36d06036e3ca8d7e3d5d367f215
Noble <neluwah@gmail.com>
2023-08-15 10:20:30 -0700

    Update: hsh.c

--- hsh.c
+++ hsh.c
@@ -8,52 +8,49 @@
  */
 int main(void)
 {
-  char *line = NULL;
-  size_t len = 0;
-  ssize_t read;
+char *line = NULL;
+size_t length = 0;
+ssize_t read;
+pid_t child;
 
-  pid_t child;
+while (1)
+{
+printf("($) ");
 
-  char *args[] = {line, NULL};
-  
-  while (1)
-    {
-  
-  printf("($) ");
+read = getline(&line, &length, stdin);
+if (read == -1)
+{
+printf("\n");
+break;
+	}
+if (line[read - 1] == '\n')
+{
+line[read - 1] = '\0';
+}
 
-  read = getline(&line, &len, stdin);
+child = fork();
 
-  if (read == -1)
-    {
-      printf("\n");
-      break;
-    }
+if (child == -1)
+{
+perror("fork failed");
+}
+else if (child == 0)
+{
+char *as[2];
+as[0] = line;
+as[1] = NULL;
 
-  if (line[read - 1] == '\n')
-    {
-      line[read - 1] = '\0';
-    }
+execve(as[0], as, NULL);
 
-  child = fork();
-
-  if (child == -1)
-    {
-      perror("fork failed");
-    }
-  else if (child == 0)
-    {
-     char *args[] = {line, NULL};
-      execve(line, args, NULL);
-
-      perror(line);
-      exit(EXIT_FAILURE);
-    }
-  else
-    {
-      int status;
-      waitpid(child, &status, 0);
-    }
-    }
-  free(line);
-  return (0);
+perror(as[0]);
+exit(EXIT_FAILURE);
+}
+else
+{
+int status;
+waitpid(child, &status, 0);
+}
+}
+free(line);
+return 0;
 }
